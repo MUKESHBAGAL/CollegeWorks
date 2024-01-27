f666a74eed8e6b2043b13d7627d31b62ee253650
mukeshBagal <2022bit039@sggs.ac.in>
2024-01-27 12:56:10 +0530

    praticle 4 Task

---
+++ frist.java
@@ -0,0 +1,49 @@
+ class frist{
+     
+     public static int task1(int[] args){
+       for(int i=0;i<4;i++){
+       
+      }
+     
+       return args[0];
+     }
+     
+      public static int task2(int[] args){
+
+      for(int i=0;i<4;i++){
+       
+      }
+     
+       return args[3];
+     }
+      public static int task3(int[] args){
+
+      for(int i=0;i<4;i++){
+      
+      }
+       return args[2];
+       
+     }
+      public static int task4(int[] args){
+    int sum=0;
+      for(int i=0;i<4;i++){
+       sum=sum+args[i];
+      }
+       return sum;
+     
+     }
+    
+       public static void main(String[] args){
+       int b[]={ };
+      
+       int a=task1(b);
+       int e=task2(b);
+       int c=task3(b);
+       int d=task4(b);
+       System.out.println(a);
+       System.out.println(e);
+       System.out.println(c);
+       System.out.println(d);
+       }
+        
+}
